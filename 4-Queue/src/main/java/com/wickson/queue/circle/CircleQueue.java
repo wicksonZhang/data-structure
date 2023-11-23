aab13cfecf7cb65e4991069cdbc37301e4b44e86
DESKTOP-KRH17BU\minuo-java02 <wickson_zhang@163.com>
2023-11-23 18:00:39 +0800

    新增循环队列思路

--- 4-Queue/src/main/java/com/wickson/queue/circle/CircleQueue.java
+++ 4-Queue/src/main/java/com/wickson/queue/circle/CircleQueue.java
@@ -1,16 +1,70 @@
 package com.wickson.queue.circle;
 
-import com.wickson.queue.list.ArrayList;
-import com.wickson.queue.list.List;
-
 /**
  * 循环队列
  */
+@SuppressWarnings("unchecked")
 public class CircleQueue<E> {
 
+    // 数组大小
+    private int size;
 
-    private List<E> arrayList = new ArrayList<>();
+    // 头元素的索引
+    private int front;
 
+    // 数组
+    private E[] elements;
 
+    // 数组容量
+    private static final int DEFAULT_CAPACITY = 10;
+
+    // 初始化构造方法
+    public CircleQueue() {
+        this.elements = (E[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    /**
+     * 元素的数量
+     *
+     * @return int
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * 队列是否为空
+     *
+     * @return boolean
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 入队
+     *
+     * @param element 元素
+     */
+    public void enQueue(E element) {
+        elements[size] = element;
+        size++;
+    }
+
+    /**
+     * 出队
+     *
+     * @return E
+     */
+    public E deQueue() {
+        return null;
+    }
+
+    /**
+     * 获取队列的头元素
+     */
+    public E front() {
+        return elements[front];
+    }
 
 }
